2e66c936c81edefa99fdb5806e9dab50ff2e4242
logsol <logspum@gmail.com>
2015-03-19 17:57:02 +0100

    added working midi translation

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,15 +1,31 @@
 #include "MainWindow.h"
-#include "ui_mainwindow.h"
+#include "ui_MainWindow.h"
 
 #include <QStringList>
-#include "RtMidi.h"
-#include <CoreAudio/CoreAudio.h>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    try {
+        midiIn = new RtMidiIn();
+    } catch (RtMidiError &error) {
+        exit(EXIT_FAILURE);
+    }
+
+    try {
+        midiOut = new RtMidiOut();
+    } catch (RtMidiError &error) {
+        exit(EXIT_FAILURE);
+    }
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(update_for_read()));
+    timer->start(5);
+
     ui->setupUi(this);
+
     listMidiInputPorts();
     listMidiOutputPorts();
 }
@@ -17,16 +33,17 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete midiIn;
+    delete midiOut;
 }
 
 void MainWindow::listMidiInputPorts()
 {
     QStringList list;
-    RtMidiIn midiIn;
-    unsigned int nPorts = midiIn.getPortCount();
+    unsigned int nPorts = midiIn->getPortCount();
     for ( unsigned int i=0; i<nPorts; i++ ) {
       try {
-        list << QString::fromStdString(midiIn.getPortName(i));
+        list << QString::fromStdString(midiIn->getPortName(i));
       }
       catch ( RtMidiError &error ) {
         ui->statusBar->showMessage(QString::fromStdString(error.getMessage()));
@@ -37,15 +54,90 @@ void MainWindow::listMidiInputPorts()
 void MainWindow::listMidiOutputPorts()
 {
     QStringList list;
-    RtMidiOut midiOut;
-    unsigned int nPorts = midiOut.getPortCount();
+    unsigned int nPorts = midiOut->getPortCount();
     for ( unsigned int i=0; i<nPorts; i++ ) {
       try {
-        list << QString::fromStdString(midiOut.getPortName(i));
+        list << QString::fromStdString(midiOut->getPortName(i));
       }
       catch ( RtMidiError &error ) {
         ui->statusBar->showMessage(QString::fromStdString(error.getMessage()));
       }
     }
     ui->outputPorts->addItems(list);
+}
+
+void MainWindow::on_inputPorts_currentIndexChanged(int index)
+{
+    midiIn->closePort();
+    inputPortIndex = index;
+    midiIn->openPort(inputPortIndex);
+    midiIn->ignoreTypes(true, true, true);
+}
+
+void MainWindow::on_outputPorts_currentIndexChanged(int index)
+{
+    midiOut->closePort();
+    outputPortIndex = index;
+    midiOut->openPort(outputPortIndex);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    std::vector<unsigned char> outputMessage;
+    outputMessage.push_back(144); // noteon ch1
+    outputMessage.push_back(64);  // note 64
+    outputMessage.push_back(100); // vel 100
+    midiOut->sendMessage(&outputMessage);
+}
+
+void MainWindow::update_for_read()
+{
+    std::vector<unsigned char> message;
+    midiIn->getMessage(&message);
+
+    if (message.size() >= 3) {
+        unsigned int byte0 = message.at(0);
+        unsigned int byte1 = message.at(1);
+        unsigned int byte2 = message.at(2);
+        unsigned int channel;
+        bool isNoteOn = false;
+        bool isNoteOff = false;
+
+        if (byte0 >= 128 && byte0 <= 143) {
+            channel = byte0 - 127;
+            isNoteOff = true;
+        }
+        if (byte0 >= 144 && byte0 <= 159) {
+            channel = byte0 - 143;
+
+            if (byte2 == 0) {
+                isNoteOff = true;
+            } else {
+                isNoteOn = true;
+            }
+        }
+
+        if (isNoteOn || isNoteOff) {
+            if (isNoteOn) {
+                std::vector<unsigned char> pitchMessage;
+
+                // translate from key to cc value
+                float pitch = ((float) byte1) / 12.0 * 32.0;
+
+                pitchMessage.push_back(175 + selectedSample);
+                pitchMessage.push_back(43); // cc for speed on volca
+                pitchMessage.push_back(floor(pitch) - 72);
+
+                midiOut->sendMessage(&pitchMessage);
+            }
+
+            message[0] = byte0 - channel + selectedSample;
+            midiOut->sendMessage(&message);
+        }
+    }
+}
+
+void MainWindow::on_sample_currentIndexChanged(int index)
+{
+    selectedSample = index + 1;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "RtMidi.h"
+#include <CoreAudio/CoreAudio.h>
+#include <vector>
 
 namespace Ui {
 class MainWindow;
@@ -15,10 +18,24 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void catchMidi(double deltatime, std::vector<unsigned char> *message, void *userData);
+
+public slots:
+    void update_for_read();
+
 private slots:
+    void on_inputPorts_currentIndexChanged(int index);
+    void on_outputPorts_currentIndexChanged(int index);
+    void on_pushButton_clicked();
+    void on_sample_currentIndexChanged(int index);
 
 private:
     Ui::MainWindow *ui;
+    RtMidiIn* midiIn;
+    RtMidiOut* midiOut;
+    unsigned int inputPortIndex = 0;
+    unsigned int outputPortIndex = 0;
+    unsigned int selectedSample = 1;
 
     void listMidiInputPorts();
     void listMidiOutputPorts();
